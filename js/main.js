6a25831d43b0f849b5732a9a77389e5a45de9d38
ESRAAmamdoh <152865273+ESRAAmamdoh@users.noreply.github.com>
2024-01-07 19:03:38 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -19,10 +19,11 @@ function display(respose){
         <p id="dgree">${el.day.maxtemp_c } <sup>o</sup>c</p>
         <img src="${el.day.condition.icon}" width=90 height=100 alt="">
       </div>
+      <p  class="text-light fs-5">${el.day.mintemp_c} <sup>o</sup>c</p>
       <p id="state" class="text-light fs-4">${el.day.condition.text }</p>
       <div class="icons">
-        <i class="fa-solid fa-wind "> 20 k / h r</i>
-        <i class="fa-solid fa-umbrella"> 20% </i>
+        <i class="fa-solid fa-wind "> ${el.day.maxwind_kph}  k / h r</i>
+        <i class="fa-solid fa-umbrella"> ${el.day.daily_chance_of_rain} % </i>
         <i class="fa-regular fa-compass"> East</i>
       </div>
     
@@ -59,38 +60,3 @@ getWeather("el wasta");
 
 
 
-// async function search(a) {
-//     let t = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=7d77b96c972b4d119a3151101212704&q=${a}&days=3`);
-//     if (t.ok && 400 != t.status) {
-//         let a = await t.json();
-//         displayCurrent(a.location, a.current),
-//         displayAnother(a.forecast.forecastday)
-//     }
-// }
-// document.getElementById("search").addEventListener("keyup", a=>{
-//     search(a.target.value)
-// }
-// );
-// var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-// const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-// function displayCurrent(a, t) {
-//     if (null != t) {
-//         var e = new Date(t.last_updated.replace(" ", "T"));
-//         let n = `<div class="today forecast">\n 
-//            <div class="forecast-header"  id="today">\n   
-//             <div class="day">${days[e.getDay()]}</div>\n 
-//                <div class=" date">${e.getDate() + monthNames[e.getMonth()]}</div>\n  
-//                  </div> \x3c!-- .forecast-header --\x3e\n   
-//                   <div class="forecast-content" id="current">\n  
-//           <div class="location">${a.name}</div>\n  
-//             <div class="degree">\n     
-//                <div class="num">${t.temp_c}<sup>o</sup>C</div>\n      \n     
-//                   <div class="forecast-icon">\n        
-//                       <img src="https:${t.condition.icon}" alt="" width=90>\n     
-//                          </div>\t\n    \n    </div>\n 
-//                             <div class="custom">${t.condition.text}</div>\n  
-//                               <span><img src="images/icon-umberella.png" alt="">20%</span>\n\t\t\t\t\t\t\t\t<span><img src="images/icon-wind.png" alt="">18km/h</span>\n\t\t\t\t\t\t\t\t<span><img src="images/icon-compass.png" alt="">East</span>\n   
-//                                </div>\n</div>`;
-//         document.getElementById("forecast").innerHTML = n
-//     }
-// }
